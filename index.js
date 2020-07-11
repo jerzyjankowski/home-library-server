063d008b1d15b7400fb2c70c2a6b31e6d7b746c1
jjankowski <jjankowski555@gmail.com>
2020-07-11 20:12:35 +0200

    getting tags, updating book with cover (removing old one), filtering books by recommendation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const ObjectId = require('mongoose').Types.ObjectId;
 const multer  = require('multer')
 const uniqueString  = require('unique-string')
+const fs  = require('fs')
 
 const app = express();
 app.use(bodyParser.json({extended: true}));
@@ -54,6 +55,29 @@ app.get('/books', function(req, res){
     })
 });
 
+app.get('/filter-books', function(req, res){
+    console.log('xxx');
+    console.log(req.query.recommendation);
+    console.log(typeof req.query.recommendation);
+    const conditions = {
+        recommendation: {$in: getArrayOf(req.query.recommendation)},
+        // state: {$in: getArrayOf(req.query.state)},
+        // starred: {$in: getArrayOf(req.query.starred)},
+        // type: {$in: getArrayOf(req.query.type)},
+    }
+    Book.find(conditions, function (err, books) {
+        if (err) return console.error(err);
+        res.send(books.map(mapBookForReturn));
+    })
+});
+
+getArrayOf = function(queryParams) {
+    if (queryParams === null || queryParams === undefined) {
+        return [];
+    }
+    return typeof queryParams === 'string' ? [queryParams] : Array.from(queryParams);
+}
+
 app.get('/books/:bookId', function(req, res){
     Book.findById(req.params.bookId, function (err, book) {
         if (err) return console.error(err);
@@ -98,17 +122,40 @@ app.post('/books', upload.single('cover'), function (req, res, next) {
     book.save().then(() => res.status(200).end()).catch(()=> res.send("error"));
 });
 
-app.put('/books/:bookId', async function(req, res) {
-    const book = mapBookForUpdate(new Book(req.body));
-    Book.findOneAndUpdate({_id: new ObjectId(req.params.bookId)}, book, function (err, book) {
+app.put('/books/:bookId', upload.single('cover'), async function(req, res, next) {
+    const bookToUpdate = mapBookForUpdate(new Book(JSON.parse(req.body.book)));
+    console.log(bookToUpdate);
+    if (req.file) {
+        removeCoverOfBook(req.params.bookId)
+        bookToUpdate.coverUrl = 'api/books/cover/' + req.file.filename;
+    }
+    Book.findOneAndUpdate({_id: new ObjectId(req.params.bookId)}, bookToUpdate, function (err, book) {
         if (err) return res.send(err);
         res.status(200).end();
     })
 });
 
+removeCoverOfBook = async function(bookId) {
+    await Book.findById(req.params.bookId, function (err, bookOld) {
+        if (err) return console.error(err);
+        if (bookOld.coverUrl) {
+            fs.unlinkSync(__dirname + '/covers/' + bookOld.coverUrl.split('/')[3]);
+        }
+    })
+}
+
 app.get('/books/cover/:coverId', function(req, res){
     res.sendFile(__dirname + '/covers/' + req.params.coverId);
 });
+
+app.get('/tags', function(req, res) {
+    Book.find({}, 'tags', function(err, books) {
+        if (err) return console.error(err);
+        const tagsSet = books.reduce((tags, book) => {book.tags.forEach(tags.add, tags);return tags;}, new Set())
+        const tagsToReturn = [...tagsSet].sort();
+        res.send(tagsToReturn);
+    });
+})
 
 app.listen(3001, function() {
     console.log("server has started on port 3001");
