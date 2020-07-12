8ab6f505b6ab5eb273bf68a750993a665e1fec81
jjankowski <jjankowski555@gmail.com>
2020-07-12 15:17:05 +0200

    added archiving, getting lists for publishers, sourceNames, categories, subCategories and tags based on db values; with categories, subCategories and tags with preconfigured lists

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json({extended: true}));
 mongoose.connect('mongodb://localhost:27017/books', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useFindAndModify', false);
 
+const caseInsensitiveComparator = (x, y) => x.toLowerCase().localeCompare(y.toLowerCase());
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, __dirname + '/covers')
@@ -27,6 +29,7 @@ const bookSchema = new mongoose.Schema({
     recommendation: String,
     state: String,
     starred: Boolean,
+    archived: Boolean,
 
     rootTitle: String,
     title: String,
@@ -65,6 +68,7 @@ app.get('/filter-books', function(req, res){
         recommendation: {$in: getArrayOf(req.query.recommendation)},
         state: {$in: getArrayOf(req.query.state)},
         starred: {$in: getArrayOf(req.query.marked)},
+        archived: {$in: getArrayOf(req.query.archived)},
         type: {$in: getArrayOf(req.query.type)},
     }
     if (req.query.tags) {
@@ -93,7 +97,7 @@ app.get('/books/:bookId', function(req, res){
 mapBookForReturn = function(book) {
     return {
         id: book._id,
-        type: book.type, recommendation: book.recommendation, state: book.state, starred: book.starred,
+        type: book.type, recommendation: book.recommendation, state: book.state, starred: book.starred, archived: book.archived,
         rootTitle: book.rootTitle, title: book.title,
         authors: book.authors,
         coverUrl: book.coverUrl,
@@ -109,7 +113,7 @@ mapBookForReturn = function(book) {
 
 mapBookForUpdate = function(book) {
     return {
-        type: book.type, recommendation: book.recommendation, state: book.state, starred: book.starred,
+        type: book.type, recommendation: book.recommendation, state: book.state, starred: book.starred, archived: book.archived,
         rootTitle: book.rootTitle, title: book.title,
         authors: book.authors,
         coverUrl: book.coverUrl,
@@ -155,7 +159,7 @@ fillBookTagsWithCategoryAndSubcategoryAndSort = function(book) {
         book.tags.push(book.subCategory)
     }
 
-    book.tags.sort((x, y) => x.toLowerCase().localeCompare(y.toLowerCase()))
+    book.tags.sort(caseInsensitiveComparator)
 }
 
 removeCoverOfBook = async function(bookId) {
@@ -177,6 +181,60 @@ app.get('/tags', function(req, res) {
         const tagsSet = books.reduce((tags, book) => {book.tags.forEach(tags.add, tags);return tags;}, new Set())
         const tagsToReturn = [...tagsSet].sort();
         res.send(tagsToReturn);
+    });
+})
+
+const preconfiguredTags = ['Android', 'Angular', 'AngularJS', 'Arduino', 'Axure UX', 'Backend', 'Backend', 'Blender',
+    'Bootstrap', 'C lang', 'C#', 'C++', 'Clouds', 'Console', 'Data', 'Data Science', 'DBs', 'DevOps', 'Django',
+    'dotNET', 'ElasticSearch', 'Flask', 'Frontend', 'Games', 'Git', 'Go', 'Groovy', 'HTML and CSS', 'Hybrid Mobile',
+    'IoT', 'Java', 'JavaFX', 'JavaScript', 'Jira', 'jQuery', 'LaTeX', 'Linux', 'MariaDB', 'Maven', 'MeteorJS',
+    'Microservices', 'Mobile', 'MongoDB', 'MySQL', 'Network', 'Node.js', 'Objective-C', 'OpenCV', 'Other', 'PHP', 'PM',
+    'PostgreSQL', 'PowerShell', 'Python', 'R', 'Rails', 'Raspberry Pi', 'ReactJS', 'Ruby', 'Scala', 'Security', 'Spark',
+    'Spring', 'Swift', 'Testing', 'TypeScript', 'Unity', 'Unreal Engine', 'UX Design', 'Vue.js', 'WebGL', 'Wordpress',
+    'Xamarin'];
+const preconfiguredAvailableCategories = ['Backend', 'Frontend', 'Mobile', 'Data', 'DevOps', 'IoT', 'Other'];
+const preconfiguredAvailableSubcategories = {
+    'Backend': ['C lang', 'C#', 'C++', 'Django', 'dotNET', 'Flask', 'Go', 'Groovy', 'Java', 'JavaFX', 'Node.js', 'PHP', 'Python', 'R', 'Rails', 'Ruby', 'Scala', 'Spring'],
+    'Frontend': ['Angular', 'AngularJS', 'Bootstrap', 'HTML and CSS', 'JavaScript', 'jQuery', 'MeteorJS', 'ReactJS', 'TypeScript', 'Vue.js', 'WebGL', 'Wordpress'],
+    'Mobile': ['Android', 'Hybrid Mobile', 'Objective-C', 'Swift', 'Xamarin'],
+    'Data': ['Data Science', 'DBs', 'ElasticSearch', 'MongoDB', 'MySQL', 'MariaDB', 'PostgreSQL'],
+    'DevOps': ['Clouds', 'Console', 'DevOps', 'Git', 'Linux', 'Maven', 'Microservices', 'Network', 'PowerShell', 'Security'],
+    'IoT': ['Arduino', 'IoT', 'Raspberry Pi'],
+    'Other': [ 'Axure UX', 'Blender', 'Games', 'Jira', 'LaTeX', 'OpenCV', 'Other', 'PM', 'Spark', 'Testing', 'Unity', 'Unreal Engine', 'UX Design']
+};
+app.get('/book-lists', function(req, res) {
+    const preconfiguredSubCategories = {};
+    preconfiguredAvailableCategories.forEach(category => preconfiguredSubCategories[category] = new Set([...preconfiguredAvailableSubcategories[category]]));
+    Book.find({}, 'tags publisher sources category subCategory', function(err, books) {
+        if (err) return console.error(err);
+        const sets = books.reduce(
+            (available, book) => {
+                available.publishers.add(book.publisher);
+                book.sources.forEach(source => available.sourceNames.add(source.name));
+                if (!available.categories.has(book.category)) {
+                    available.categories.add(book.category);
+                    available.subCategories[book.category] = new Set();
+                }
+                available.subCategories[book.category].add(book.subCategory);
+                book.tags.forEach(available.tags.add, available.tags);
+                return available;
+            },
+            {
+                publishers: new Set(),
+                sourceNames: new Set(),
+                categories: new Set(preconfiguredAvailableCategories),
+                subCategories: preconfiguredSubCategories,
+                tags: new Set(preconfiguredTags)
+            }
+        )
+        sets.categories.forEach(category => sets.subCategories[category] = [...sets.subCategories[category]].sort())
+        res.send({
+            publishers: [...sets.publishers].sort(caseInsensitiveComparator),
+            sourceNames: [...sets.sourceNames].sort(caseInsensitiveComparator),
+            categories: [...sets.categories].sort(caseInsensitiveComparator),
+            subCategories: sets.subCategories,
+            tags: [...sets.tags].sort(caseInsensitiveComparator),
+        });
     });
 })
 
